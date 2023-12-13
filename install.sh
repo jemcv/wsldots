aec631b0160be5a00041f97d363267c3a2bbdf99
jemcv <41255513+jemcv@users.noreply.github.com>
2023-12-14 01:15:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,8 @@ chsh -s $(which zsh)
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install Starship
+curl -sS https://starship.rs/install.sh | sh
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
